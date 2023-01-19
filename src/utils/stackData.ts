d0e2210bd9da1e5a2994820ea0aa8d6fb58a634d
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-01-19 16:31:56 -0300

    feat: add own stacks and user data

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,40 +1,47 @@
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaGit, FaPython } from "react-icons/fa";
+import { DiScrum } from "react-icons/di";
 import {
-  FaDocker,
-  FaHtml5,
   FaCss3Alt,
+  FaDocker,
+  FaGit,
+  FaHtml5,
   FaJs,
   FaNode,
+  FaPython,
   FaReact,
 } from "react-icons/fa";
+import { RxStitchesLogo } from "react-icons/rx";
+import {
+  SiDjango,
+  SiExpress,
+  SiMaterialui,
+  SiMongodb,
+  SiPostgresql,
+  SiPrisma,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
+  { title: "HTML", img: FaHtml5 },
+  { title: "CSS", img: FaCss3Alt },
+  { title: "JS", img: FaJs },
   { title: "TypeScript", img: SiTypescript },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
-  {
-    title: "React",
-    img: FaReact,
-  },
-  { title: "Next.js", img: TbBrandNextjs },
   { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: SiPostgresql },
-  { title: "Git", img: FaGit },
+  { title: "Node JS", img: FaNode },
+  { title: "React", img: FaReact },
+  { title: "Next.js", img: TbBrandNextjs },
+  { title: "Material UI", img: SiMaterialui },
+  { title: "Styled Components", img: SiStyledcomponents },
+  { title: "Tailwind", img: SiTailwindcss },
+  { title: "Stiches", img: RxStitchesLogo },
+  { title: "Express", img: SiExpress },
   { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
+  { title: "Prisma ORM", img: SiPrisma },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "MongoDB", img: SiMongodb },
+  { title: "Git", img: FaGit },
+  { title: "Scrum", img: DiScrum },
 ];
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Giuseppe Dias",
+  githubUser: "galio17",
+  linkedinUser: "giuseppe-dias",
+  whatsappNumber: "+5596984141178",
+  emailUser: "giuseppe.dias17@gmail.com.br",
+  instagramUser: "galio_cadmio",
+  facebookUser: "giuseppe.armando",
 };
